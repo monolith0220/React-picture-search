3b7de454114ad9c9314f6a00386be7257c4d0116
monolith0220 <funky.atmos.worlddd@gmail.com>
2022-03-21 02:08:37 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const [images, setImages] = useState([]);
+	const [text, setText] = useState("");
+	const [query, setQuery] = useState("cat");
 
-export default App;
+	useEffect(() => {
+		console.log("useEffectが走りました");
+		fetch(
+			`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setImages(data.results);
+			});
+	}, [query]);
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		setQuery(text);
+		setText("");
+		console.log("onSubmit");
+	};
+
+	return (
+		<div className="App">
+			<div className="main">
+				<form onSubmit={onSubmit}>
+					<input
+						type="text"
+						onChange={(e) => setText(e.target.value)}
+						value={text}
+					/>
+					<button type="submit">Search</button>
+				</form>
+			</div>
+			<div className="container">
+				{images.map((image) => (
+					<div key={image.id} className="card">
+						<img src={image.urls.regular} className="card-img" alt="" />
+						<div className="card-contet">
+							<div className="card-title">{image.alt_description}</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
